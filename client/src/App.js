91b3707a0ba0724a0ed350255de6f5e0b5554a5f
Faryal Ansari <faryalansari@live.com>
2021-03-02 14:35:02 -0500

    add approtiate dependencies & create path in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 import './App.css'
 import { Route, Switch } from 'react-router-dom'
 import Nav from './components/Nav'
+import Home from './pages/Home'
+import ViewCities from './pages/ViewCities'
+import PostListing from './pages/PostListing'
+import ViewAllListing from './pages/ViewAllListing'
 
 export default class App extends Component {
   render() {
@@ -11,15 +15,13 @@ export default class App extends Component {
 
         <main>
           <Switch>
-            <Route />
-            <Route />
-            <Route />
-            <Route />
+            <Route exact path="/" component={Home} />
+            <Route path="/view/cities/" component={ViewCities} />
+            <Route path="/post/listing" component={PostListing} />
+            <Route path="/view/all/listing" component={ViewAllListing} />
           </Switch>
         </main>
       </div>
     )
   }
 }
-
-export default App
