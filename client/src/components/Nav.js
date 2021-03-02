dfd830fdfb0798e735b0d338d91bf3f182346426
Faryal Ansari <faryalansari@live.com>
2021-03-02 14:46:26 -0500

    NavLink set and functional

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
 
 export default class Nav extends Component {
   render() {
     return (
-      <div>
-        <h1>Nav Bar</h1>
-      </div>
+      <header>
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/view/cities/">Cities</NavLink>
+          <NavLink to="/post/listing">Post Listing</NavLink>
+          <NavLink to="/view/all/listing">View All Listings</NavLink>
+        </nav>
+      </header>
     )
   }
 }
