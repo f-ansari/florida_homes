95f624cdef81d434a41bd0a9f288d389e0f53147
Faryal Ansari <faryalansari@live.com>
2021-03-03 00:45:27 -0500

    listing detail page populates data

--- client/src/pages/ListingDetails.js
+++ client/src/pages/ListingDetails.js
@@ -7,7 +7,7 @@ export default class ListingDetails extends Component {
     super(props)
     this.state = {
       propertyId: props.match.params.propertyId,
-      propertyDetail: {}
+      propertyDetails: {}
     }
   }
 
@@ -16,14 +16,13 @@ export default class ListingDetails extends Component {
   }
 
   getPropertyDetails = async () => {
-    // const propertyId = this.props.match.params.id
     console.log(this.state.propertyId, 'id')
     try {
       console.log('above res')
       const res = await axios.get(
         `${BASE_URL}/api/properties/details/${this.state.propertyId}`
       )
-      this.setState({ propertyDetail: res.data.property })
+      this.setState({ propertyDetails: res.data.property })
       console.log(res, 'res-axios')
     } catch (error) {
       throw error
@@ -31,10 +30,45 @@ export default class ListingDetails extends Component {
   }
 
   render() {
-    // console.log(this.props, 'listingD')
+    const {
+      image,
+      price,
+      street,
+      city,
+      state,
+      zipCode,
+      bed,
+      bath,
+      sqft
+    } = this.state.propertyDetails
     return (
       <div>
         <h1>Listing Details</h1>
+        <section image-container className="image-container">
+          <div>
+            <img src={image} alt="poster" />
+          </div>
+        </section>
+
+        <section className="details">
+          <div className="flex-row space"></div>
+          <div>
+            <h1>${price}</h1>
+          </div>
+
+          <div>
+            <h3>{street}</h3>
+            <h4>
+              {city}, {state}, {zipCode}
+            </h4>
+          </div>
+
+          <div>
+            <h4>Bed: {bed}</h4>
+            <h4>Bath: {bath}</h4>
+            <h4>Sqft: {sqft}</h4>
+          </div>
+        </section>
       </div>
     )
   }
