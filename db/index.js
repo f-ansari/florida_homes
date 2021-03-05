87ea0f7ec5059ac4919132206e011e22334805ed
Faryal Ansari <faryalansari@live.com>
2021-03-05 16:46:43 -0500

    ready for heroku

--- db/index.js
+++ db/index.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose')
-
-let MONGODB_URI =
-  process.env.PROD_MONGODB ||
-  process.env.MONGODB_URI ||
-  'mongodb://127.0.0.1:27017/floridaHomesDatabase'
-
+require('dotenv').config()
+let dbUrl =
+  process.env.NODE_ENV === 'production'
+    ? process.env.MONGODB_URI
+    : 'mongodb://127.0.0.1:27017/floridaHomesDatabase'
 mongoose
-  .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
+  .connect(dbUrl, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: true
+  })
   .then(() => {
-    console.log('Sucessfully connected to MongoDB.')
+    console.log('Successfully connected to MongoDB.')
   })
   .catch((e) => {
     console.error('Connection error', e.message)
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const db = require('./db')
 const bodyParser = require('body-parser')
 const loggger = require('morgan')
 const cors = require('cors')
+const path = require('path')
 
 const PORT = process.env.PORT || 3001
 
@@ -15,5 +16,12 @@ app.use(cors())
 app.use('/api', routes)
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/client/build/index.html`))
+  })
+}
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
